84d57d2e8439721d26661107bb986e2b690dcac7
R0binicus <102570571+R0binicus@users.noreply.github.com>
2023-05-03 19:23:53 +1000

    Working on Program Structure

---
+++ Assignment backup/src/solver/TruthTable.java
@@ -0,0 +1,17 @@
+package solver;
+import java.util.*;
+
+public class TruthTable extends CheckingMethod {
+	
+	public TruthTable()
+	{
+		code = "TT";
+		longName = "Truth Table";
+	}
+	
+	@Override
+	public void Solve() {
+		System.out.println("The truth table just ran succesfully!");
+	}
+}
+
---
+++ Assignment/InferenceEngine.java
@@ -0,0 +1,3 @@
+package solver;
+import java.io.*;
+import java.util.*;
