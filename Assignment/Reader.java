be8846221d946459770b988af87907d3d3a38eaa
IamScrump <100543055+IamPeachtree@users.noreply.github.com>
2023-05-04 11:42:29 +1000

    reader stuff
    A bit of work that I was able to do before class

---
+++ Assignment/Reader.java
@@ -0,0 +1,25 @@
+// I am not sure what we might need to import so this is blank for now
+
+public class Reader
+{
+    // requires file name on creation of object
+    public static void Reader(string file)
+    {   // on run extract
+        extract(file);
+
+    }
+
+    private string extract(string file)
+    {
+        // line 1 - keyword
+        string keyword = a;
+        // line 2 - knowledgeBase
+        string kb = b;
+        // line 3 - proposition
+        string proposition = c;
+        // line 4 - qEntail
+        qEntail = d;
+        return a,b,c,d;
+    }
+
+}
