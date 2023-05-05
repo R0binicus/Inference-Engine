6c1e8dedadff6f2cc219ae4d2375e7afe5404e80
IamScrump <100543055+IamPeachtree@users.noreply.github.com>
2023-05-05 14:22:12 +1000

    Reader
    I have done some work on this. I dunno if this will work but I have included the resources I have used so you can see were my head is at.

--- Assignment/Reader.java
+++ Assignment/Reader.java
@@ -1,25 +1,39 @@
-// I am not sure what we might need to import so this is blank for now
+//Recourses used for code
+//05/05/2023
+// https://www.digitalocean.com/community/tutorials/java-read-file-line-by-line
+// https://jenkov.com/tutorials/java/fields.html
+// https://www.geeksforgeeks.org/system-out-println-in-java/
+// https://www.geeksforgeeks.org/difference-between-print-and-println-in-java/
 
-public class Reader
-{
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Reader {
+    List<string> _horn;
     // requires file name on creation of object
-    public static void Reader(string file)
-    {   // on run extract
-        extract(file);
+    public Reader(string file){   // on run extract
+       _horn = extract(file);
 
     }
-
-    private string extract(string file)
-    {
-        // line 1 - keyword
-        string keyword = a;
-        // line 2 - knowledgeBase
-        string kb = b;
-        // line 3 - proposition
-        string proposition = c;
-        // line 4 - qEntail
-        qEntail = d;
-        return a,b,c,d;
+    //get list of values
+    private List <string> extract(string file) {
+        try {
+            // create a list of string for each line in the file given its path
+            List<string> query = Files.readAllLines(Path.get(file));
+            return query;
+        }
+        //on error
+        catch (IOExeption issue) {
+            issue.printStackTrace();
+        }
+        return null;
     }
 
+    public void string testField() {
+        for(string position : _horn) {
+            System.out.println(position);
+        }
+    }
 }
