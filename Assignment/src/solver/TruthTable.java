487b3ed04398cb03a597eee2e81b2e6f476b0220
IamPeachtree <100543055+IamPeachtree@users.noreply.github.com>
2023-05-08 16:49:41 +1000

    more work

--- Assignment/src/solver/TruthTable.java
+++ Assignment/src/solver/TruthTable.java
@@ -2,7 +2,9 @@ package solver;
 import java.util.*;
 
 // https://www.freecodecamp.org/news/javascript-split-how-to-split-a-string-into-an-array-in-js/
-
+// https://www.w3schools.com/java/java_arraylist.asp
+// https://www.geeksforgeeks.org/array-get-method-in-java/
+// https://www.appsdeveloperblog.com/java-stream-count-operation/
 /* pseudocode for the truth table
 TT-E NTAILS ?(KB , α) returns true or false
 inputs: KB , the knowledge base, a sentence in propositional logic
@@ -30,23 +32,44 @@ public class TruthTable extends CheckingMethod {
 		longName = "Truth Table";
 	}
 
-	public static boolean TTEntails(String KBString, String Query)
+	public static boolean TTEntails(String KBString, String Alpha)
 	{	// I think a is 'd' in the test
 		//symbols <-- list of probositions in kb and a
-		List<String> KB = GetPropositions(String KBString, String Query);
+		List<String> KB = GetPropositions(String KBString, String Alpha);
+		List<String> Symbols = GetSymbols(var KB, String Alpha)
+		TTCheckAll(KB);
 	}
 
-	// create the lsit
-	private List <String> GetPropositions(String KBString, String Query)
+	private List GetSymbols (List <String> KB, String Alpha){
+
+	List<String> Symbols = new ArrayList<>();
+		for(int i = 0; i < KB.stream().count()){
+
+			string sym = KB.get(i);
+			// somthing about the symbols remove stuff or somthing idk. I think we will use this to constriucth the logic
+			symbols.add(sym);
+		}
+	return symbols;
+	}
+	private static boolean TTCheckAll(KB){
+
+		while (!KB.isEmpty()){
+			// remove first item in arraylist
+			string check = KB.remove(0);
+		}
+
+	}
+
+	// create the list
+	private List <String> GetPropositions(String KBString, String Alpha)
 	{
 		List<String> KB = new ArrayList<>();
 		 String[] sections = KBstring.split(";");
 		 for(String arg : sections)
-		 {
+		 {	// remove white spaces
 			 KB.add(arg.trim());
-
 		 }
-		// split KBString by ; and add to List
+		 // return the list
 	return KB;
 	}
 
