0dde153704a4a8657503ffdcfbee6afd074e7dbb
IamPeachtree <100543055+IamPeachtree@users.noreply.github.com>
2023-05-12 15:35:30 +1000

    more work

--- Assignment/src/solver/InferenceEngine.java
+++ Assignment/src/solver/InferenceEngine.java
@@ -1,88 +1,84 @@
 package solver;
+
 import java.io.*;
 import java.util.*;
 //import time;
 
 /**
  * nPuzzler --- main class/initiate search methods/read problem file/etc.
- * @author    COS30019
+ * 
+ * @author COS30019
  */
-class InferenceEngine
-{
+class InferenceEngine {
 
-	//the number of methods programmed into nPuzzler
+	// the number of methods programmed into nPuzzler
 	public static final int METHOD_COUNT = 2;
-	//public static nPuzzle gPuzzle;
+	// public static nPuzzle gPuzzle;
 	public static CheckingMethod[] lMethods;
 
 	static ArrayList<HornClause> ClauseList = new ArrayList<>();
 
+	public static void main(String[] args) {
 
-	public static void main(String[] args)
-	{
-		
 		long startTime = System.currentTimeMillis();
-		//Create method objects
+		// Create method objects
 		InitMethods();
-		
-		//args contains:
-		//  [0] - filename containing puzzle(s)
-		//  [1] - method name
-		
-		if(args.length < 2) {
-			System.out.println("Usage: nPuzzler <filename> <search-method>.");
-			System.exit(1);			
+
+		// args contains:
+		// [0] - filename containing puzzle(s)
+		// [1] - method name
+
+		if (args.length < 2) {
+			System.out.println("Usage: Inference Engine <filename> <search-method>.");
+			System.exit(1);
 		}
-		
-		//Get the puzzle from the file
+
+		// Get the puzzle from the file
 		System.out.println(readProblemFile(args[0]));
-		//readProblemFile(args[0]);
-		
+		// readProblemFile(args[0]);
+
 		String method = args[1];
 		CheckingMethod thisMethod = null;
-		
-		//determine which method the user wants to use to solve the puzzles
-		for(int i = 0; i < METHOD_COUNT; i++)
-		{
-			//do they want this one?
-			if(lMethods[i].code.compareTo(method) == 0)
-			{
-				//yes, use this method.
+
+		// determine which method the user wants to use to solve the puzzles
+		for (int i = 0; i < METHOD_COUNT; i++) {
+			// do they want this one?
+			if (lMethods[i].code.compareTo(method) == 0) {
+				// yes, use this method.
 				thisMethod = lMethods[i];
 			}
 		}
-		
-		//Has the method been implemented?
-		if(thisMethod == null)
-		{
-			//No, give an error
-			System.out.println("Search method identified by " + method + " not implemented. Methods are case sensitive.");
+
+		// Has the method been implemented?
+		if (thisMethod == null) {
+			// No, give an error
+			System.out
+					.println("Search method identified by " + method + " not implemented. Methods are case sensitive.");
 			System.exit(1);
 		}
-		
-		//Solve the puzzle, using the method that the user chose
+
+		// Solve the puzzle, using the method that the user chose
 		thisMethod.Solve();
 
 		long endTime = System.currentTimeMillis();
 		System.out.println("That took " + (endTime - startTime) + " milliseconds");
 		System.exit(0);
 	}
-	
-	private static void InitMethods()
-	{
+
+	private static void InitMethods() {
 		lMethods = new CheckingMethod[METHOD_COUNT];
 		lMethods[0] = new ForwardChain();
 		lMethods[1] = new BackwardsChain();
-		//lMethods[2] = new TruthTable();
+		// lMethods[2] = new TruthTable();
 
 	}
-	
-	private static String readProblemFile(String fileName) // this allow only one puzzle to be specified in a problem file 
+
+	private static String readProblemFile(String fileName) // this allow only one puzzle to be specified in a problem
+															// file
 	{
 		String result = "";
-		try
-		{
-			//create file reading objects
+		try {
+			// create file reading objects
 			FileReader reader = new FileReader(fileName);
 			BufferedReader puzzle = new BufferedReader(reader);
 
@@ -91,94 +87,88 @@ class InferenceEngine
 			String ASK = puzzle.readLine();
 			String checkInput = puzzle.readLine();
 
-			//result = TELL + KBinput + ASK + checkInput;
-			//			
-			//puzzle.close();
-
-			//List<String> TELLList;// = new ArrayList<>();
-//
-			//String KBinput = null;
-			//String checkInput = null;
-			//int i = 100;
-			//String currentLine = puzzle.readLine().trim();
-//
-			//while (currentLine != null) 
-			//{
-			//	String TELL = "TELL";
-			//	currentLine = currentLine.trim();
-			//	System.out.println(currentLine);
-			//	if (currentLine == TELL)// && currentLine != null)
-			//	{
-			//		KBinput = puzzle.readLine();
-			//		//TELLList = ParseTell(KBinput);
-			//		System.out.println(KBinput);
-			//	}
-			//	else
-			//	{
-			//		System.out.println(TELL.length());
-			//		System.out.println(currentLine.length());
-			//	}
-//
-			//	if (currentLine == "ASK")// && currentLine != null)
-			//	{
-			//		checkInput = puzzle.readLine().trim();
-			//		System.out.println(checkInput);
-			//	}
-			//	currentLine = puzzle.readLine();
-			//}
-//
+			// result = TELL + KBinput + ASK + checkInput;
 			//
-			////String TELL = puzzle.readLine();
-			////String ASK = puzzle.readLine();
-//
-			//if (KBinput != null && checkInput != null)
-			//{
-			//	//
-			//}
-			//else 
-			//{
-			//	System.out.println("Could not find TELL or ASK line, please reformat the file");
-			//	System.exit(1);
-			//}
+			// puzzle.close();
+
+			// List<String> TELLList;// = new ArrayList<>();
+			//
+			// String KBinput = null;
+			// String checkInput = null;
+			// int i = 100;
+			// String currentLine = puzzle.readLine().trim();
+			//
+			// while (currentLine != null)
+			// {
+			// String TELL = "TELL";
+			// currentLine = currentLine.trim();
+			// System.out.println(currentLine);
+			// if (currentLine == TELL)// && currentLine != null)
+			// {
+			// KBinput = puzzle.readLine();
+			// //TELLList = ParseTell(KBinput);
+			// System.out.println(KBinput);
+			// }
+			// else
+			// {
+			// System.out.println(TELL.length());
+			// System.out.println(currentLine.length());
+			// }
+			//
+			// if (currentLine == "ASK")// && currentLine != null)
+			// {
+			// checkInput = puzzle.readLine().trim();
+			// System.out.println(checkInput);
+			// }
+			// currentLine = puzzle.readLine();
+			// }
+			//
+			//
+			//// String TELL = puzzle.readLine();
+			//// String ASK = puzzle.readLine();
+			//
+			// if (KBinput != null && checkInput != null)
+			// {
+			// //
+			// }
+			// else
+			// {
+			// System.out.println("Could not find TELL or ASK line, please reformat the
+			// file");
+			// System.exit(1);
+			// }
 
 			result = KBinput + "         " + checkInput;
 			List<String> printTest = ParseTell(KBinput);
 
-			for(String arg : printTest)
-		 	{	// remove white spaces
-				//System.out.println(arg + "    ");
-		 	}
-						
+			for (String arg : printTest) { // remove white spaces
+											// System.out.println(arg + " ");
+			}
+
 			puzzle.close();
-			
-		}
-		catch(FileNotFoundException ex)
-		{
-			//The file didn't exist, show an error
+
+		} catch (FileNotFoundException ex) {
+			// The file didn't exist, show an error
 			System.out.println("Error: File \"" + fileName + "\" not found.");
 			System.out.println("Please check the path to the file.");
 			System.exit(1);
-		}
-		catch(IOException ex)
-		{
-			//There was an IO error, show and error message
-			System.out.println("Error in reading \"" + fileName + "\". Try closing it and programs that may be accessing it.");
+		} catch (IOException ex) {
+			// There was an IO error, show and error message
+			System.out.println(
+					"Error in reading \"" + fileName + "\". Try closing it and programs that may be accessing it.");
 			System.out.println("If you're accessing this file over a network, try making a local copy.");
 			System.exit(1);
 		}
 		return result;
 	}
 
-
-	private static List<String> ParseTell(String TELLString)
-	{
+	private static List<String> ParseTell(String TELLString) {
 		List<String> TELLList = new ArrayList<>();
-		 String[] sections = TELLString.split(";");
-		 for(String arg : sections)
-		 {	// remove white spaces
-			 TELLList.add(arg.trim());
-		 }
-		
+		String[] sections = TELLString.split(";");
+		for (String arg : sections) { // remove white spaces
+			TELLList.add(arg.trim());
+		}
+
 		return TELLList;
 	}
 }
--- Assignment/src/solver/KnowledgeBase.java
+++ Assignment/src/solver/KnowledgeBase.java
@@ -1,66 +1,57 @@
 package solver;
 
 // https://www.w3schools.com/java/java_arraylist.asp
+//https://www.w3schools.com/java/java_linkedlist.asp
 // I need this here for these lists according to internet overlord
 import java.util.ArrayList;
 import java.util.*;
 
 public class KnowledgeBase {
     // fields
-    private ArrayList<HornClause> horny;
+    private ArrayList<HornClause> horny = new ArrayList<HornClause>();
     // I have seen facts as a thing in similar programs online so i think we will
     // need this
-    private ArrayList<String> hornyFacts;
-    // Symbols list as the pseudocode says
-    private ArrayList<String> hornySymbols;
+    private ArrayList<String> hornyFacts = new ArrayList<String>();
+    // Symbols list as the pseudocode says. im not sure what we need to do with this
+    // right now but it is a thing
+    private ArrayList<String> hornySymbols = new ArrayList<String>();
 
     // kb constructor
-    public class KnowledgeBase()
-    {
+    public KnowledgeBase() {
+    }
 
-        }
+    public ArrayList<String> getHorny() {
 
-        public ArrayList<HornClause> getHorny() {
+    }
 
-        }
-
-        ic ArrayList<String> getHornySymbols(){
-        //check if symbol is in list. if it is not add to list
-        foreach(HornClause hornyC: horny)
-        {   //negation
-            if(hornyC.contains("~") && !hornySymbols.contains("~"))
-            {
+    public ArrayList<String> getHornySymbols() {
+        // check if symbol is in list. if it is not add to list
+        for (HornClause hornyC : horny)
+            ;
+        { // negation
+            if (hornyC.contains("~") && !hornySymbols.contains("~")) {
                 hornySymbols.add("~");
-            }//conjunction
-            if(hornyC.contains("&")&& !hornySymbols.contains("&"))
-            {
+            } // conjunction
+            if (hornyC.contains("&") && !hornySymbols.contains("&")) {
                 hornySymbols.add("&");
-            }//disjunction
-            if(hornyC.contains("||") && !hornySymbols.contains("||"))
-            {
+            } // disjunction
+            if (hornyC.contains("||") && !hornySymbols.contains("||")) {
                 hornySymbols.add("||");
-            }//implication
-            if(hornyC.contains("=>") && !hornySymbols.contains("=>"))
-            {
+            } // implication
+            if (hornyC.contains("=>") && !hornySymbols.contains("=>")) {
                 hornySymbols.add("=>");
-            }//bi-conditional
-            if(hornyC.contains("<=>") && !hornySymbols.contains("<=>"))
-            {
+            } // bi-conditional
+            if (hornyC.contains("<=>") && !hornySymbols.contains("<=>")) {
                 hornySymbols.add("<=>");
             }
         }
     }
-    /
 
-        public ArrayList<String> getHornyFacts() {
-        foreach(HornClause hornyC : horny)
-        {
-            if(hornyC )
-            {
+    public ArrayList<String> getHornyFacts() {
+        for (String hornyC : horny) {
+            if (hornyC) {
                 hornyFacts.add(hornyC);
-            }      
+            }
         }
     }
-
-    public 
 }
