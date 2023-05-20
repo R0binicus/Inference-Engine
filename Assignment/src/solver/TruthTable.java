100f688713a7ca48aff9459ed48f2b1d4f5ae852
IamPeachtree <100543055+IamPeachtree@users.noreply.github.com>
2023-05-20 13:28:33 +1000

    TT uploaded
    do not know if it will solve properly

--- Assignment/src/solver/TruthTable.java
+++ Assignment/src/solver/TruthTable.java
@@ -6,34 +6,18 @@ import java.util.*;
 // https://www.w3schools.com/java/java_arraylist.asp
 // https://www.geeksforgeeks.org/array-get-method-in-java/
 // https://www.appsdeveloperblog.com/java-stream-count-operation/
-/* pseudocode for the truth table
-TT-E NTAILS ?(KB , α) returns true or false
-inputs: KB , the knowledge base, a sentence in propositional logic
-α, the query, a sentence in propositional logic
-symbols ← a list of the proposition symbols in KB and α
-return TT-C HECK -A LL (KB , α, symbols, { })
-
-function TT- CHECK -A LL (KB, α, symbols , model ) returns true or false
-if EMPTY ?(symbols) then
-	if PL-TRUE ?(KB , model ) then return PL-T RUE ?(α, model )
-	else return true // when KB is false, always return true
-else do
-P ← F IRST (symbols)
-rest ← R EST (symbols)
-return (TT-C HECK -A LL (KB, α, rest, model ∪ {P = true}) and
-	TT-C HECK -A LL (KB , α, rest, model ∪ {P = false }))
- */
 
 public class TruthTable extends CheckingMethod {
 	// this is here for ease of testing and will be removed later.
 
-	ArrayList<String> _ttkb;
+	HashMap<String, List<Integer>> _ttkb;
 
 	public void TT(List<String> horn, List<String> symbols, String query) {
 
 		var unique = getTTCount(symbols);
-		var emptyTT = generateTT(unique);
-		System.out.println(emptyTT);
+		var TT = generateTT(unique);
+		// System.out.println(TT);
+		_ttkb = TT;
 		// return _ttkb;
 	}
 
@@ -44,9 +28,11 @@ public class TruthTable extends CheckingMethod {
 				unique.add(s);
 			}
 		}
-		for (String me : unique) {
-			System.out.println("unique: " + me);
-		}
+		/*
+		 * for (String me : unique) {
+		 * System.out.println("unique: " + me);
+		 * }
+		 */
 		System.out.println(unique);
 
 		return unique;
@@ -56,29 +42,91 @@ public class TruthTable extends CheckingMethod {
 		HashMap<String, List<Integer>> TT = new HashMap<String, List<Integer>>();
 		// +1 for the query
 		int colnum = unique.size();
-		Double power = Math.pow(2, colnum);
-		int powerInt = power.intValue();
-		// int listSize = (powerInt / colnum);
-		System.out.println("Size of each " + powerInt);
+		int power = (int) Math.pow(2, colnum);
+		int rowcount = power / colnum;
+		// test if correct n is used
+		// System.out.println("Size of each " + power);
 
 		for (int i = 0; i < colnum; i++) {
 			// add data into HashMap
-			TT.put(unique.get(i), new ArrayList<Integer>(powerInt));
+			TT.put(unique.get(i), new ArrayList<Integer>());
 		}
 		// fill facts
 		for (String s : TT.keySet()) {
 			// all parts that do not contain an and statement. The and statement is reliant
 			// on these being fileld first
+			var list = TT.get(s);
 			if (!s.contains("&")) {
-				for (int i = 0; i < powerInt; i++) {
-					int num = 0;
-					var list = TT.get(s);
-					list.add(num);
-				}
+				// alter this to be every combination
+				for (int i = 0; i < rowcount; i++) {
+					String num = Integer.toBinaryString(i);
 
+					// fill in leading numbers with 0 until ints length is equal to the column
+					// number (n)
+					while (num.length() < colnum) {
+						num = "0" + num;
+					}
+					for (int l = 0; l < colnum; l++) {
+						int value = Character.getNumericValue(num.charAt(l));
+
+						list.add(value);
+					}
+				}
+				// I was having issues where the last 2 values were not working correctly.
+				// Hopefuly this addresses the issue and does not mess with the expected outcome
+				int remaining = power - list.size();
+				for (int i = 0; i < remaining; i++) {
+					String binaryString = Integer.toBinaryString(i);
+					while (binaryString.length() < colnum) {
+						binaryString = "0" + binaryString;
+					}
+					int value = Character.getNumericValue(binaryString.charAt(i));
+					list.add(value);
+				}
 			}
 		}
-
+		// test col size is correct and & is not added to
+		/*
+		 * for (String s : TT.keySet()) {
+		 * System.out.println(s + " " + TT.get(s).size());
+		 * }
+		 */
+		// start to fill in & logic
+		for (String s : TT.keySet()) {
+			if (s.contains("&")) {
+				String[] and = s.split("&");
+				String left = and[0].trim();
+				String right = and[1].trim();
+				// test table selection
+				// System.out.println("left: " + left + " Right: " + right);
+				var self = TT.get(s);
+				var leftcol = TT.get(left);
+				var rightcol = TT.get(right);
+				// check if there is a null list. Will assume that in the case of a null list
+				// the value is false
+				if (leftcol == null || rightcol == null) {
+					for (int i = 0; i < power; i++) {
+						self.add(0);
+					}
+				} else {
+					for (int i = 0; i < power; i++) {
+						int leftval = leftcol.get(i);
+						int rightval = rightcol.get(i);
+						if (leftval == 1 && rightval == 1) {
+							self.add(1);
+						} else {
+							self.add(0);
+						}
+					}
+				}
+			}
+		}
+		// before table is returned test table count
+		/*
+		 * for (String s : TT.keySet()) {
+		 * System.out.println(s + " " + TT.get(s).size());
+		 * }
+		 */
 		return TT;
 	}
 
