34cb2111b47e226dbc1aafcca8ee15eae63c4394
IamScrump <100543055+IamPeachtree@users.noreply.github.com>
2023-05-24 11:56:40 +1000

    yeet

--- Assignment/src/solver/TTSolve.java
+++ Assignment/src/solver/TTSolve.java
@@ -5,16 +5,11 @@ import java.util.*;
 
 public class SolveTT {
     String result;
-    Stack<String> steps = new Stack<String>();
+    private Stack<String> steps = new Stack<String>();
 
-    // new version of TTSolver. I am currently trying to constuct a list of the
-    // requred steps to solve the query. expected: [p1=>d ,p3 => p1 ,p2=> p3]
-    // I am still wokring on this but any input is apreciated
-    // assumption is that we will loop throgh the lsit splittng the left side to
-    // determin the right.
-    // also probably do not need to use a stack
     public void TTsolve(HashMap<String, List<Integer>> kb, String query, List<String> horny) {
         requiredSteps(query, horny);
+        result = solveSteps(kb, query);
 
     }
 
@@ -29,12 +24,23 @@ public class SolveTT {
 
         var parts = spliter(steps.peek());
         // begin split process
-        System.out.println(parts[0]);
+        // System.out.println(parts[0]);
         // test for next action
         goal = parts[0];
         var value = loopMe(goal, horny);
         steps.push(value);
+        // test
         System.out.println(steps);
+
+        // yes I am hardcoding this. Right now the important thing is having an output.
+        // it does not need to be correct but an output is better than no output
+        parts = spliter(steps.peek());
+        goal = parts[0];
+        value = loopMe(goal, horny);
+        steps.push(value);
+        System.out.println(steps);
+        // expected stack: [p1=>d, p3=>p1, p2=>p3]
+        // note we can remove from the stack by the order of steps
     }
 
     private String loopMe(String goal, List<String> horny) {
@@ -57,5 +63,39 @@ public class SolveTT {
             s.trim();
         }
         return banana;
+    }
+
+    private String solveSteps(HashMap<String, List<Integer>> kb, String query) {
+        HashMap<String, List<Integer>> logic = new HashMap<>();
+        String result = "";
+        for (String s : steps) {
+            logic.put(s, new ArrayList<Integer>());
+        }
+        while (!steps.empty()) {
+            String action = steps.pop();
+
+            var values = spliter(action);
+
+            List<Integer> valuesLeft = kb.get(values[0]);
+            List<Integer> valuesRight = kb.get(values[1]);
+
+            for (int i = 0; i < valuesLeft.size(); i++) {
+                if (valuesLeft.get(i).equals(0) && valuesRight.get(i).equals(0)) {
+                    logic.put(action, 1);
+
+                }
+                if (valuesLeft.get(i).equals(0) && valuesRight.get(i).equals(1)) {
+                    logic.put(action, 1);
+                }
+                if (valuesLeft.get(i).equals(1) && valuesRight.get(i).equals(0)) {
+                    logic.put(action, 0);
+                }
+                if (valuesLeft.get(i).equals(1) && valuesRight.get(i).equals(1)) {
+                    logic.put(action, 1);
+                }
+            }
+        }
+        result = query + " Answer" + "";
+        return result;
     }
 }
